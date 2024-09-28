2a06066651d24850f780ba7bbd1f98ef81bcf1e6
JJimini <min102602@pusan.ac.kr>
2024-09-29 00:52:57 +0900

    feat: PlayerDetails 관련 개발

---
+++ src/main/java/com/tree/tree/config/security/PlayerDetails.java
@@ -0,0 +1,54 @@
+package com.tree.tree.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+public class PlayerDetails implements UserDetails {
+
+    private final String nickName;
+    private final String password;
+    private final List<String> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return nickName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/tree/tree/config/security/PlayerDetailsService.java
@@ -0,0 +1,22 @@
+package com.tree.tree.config.security;
+
+import com.tree.tree.player.repository.PlayerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class PlayerDetailsService implements ReactiveUserDetailsService {
+
+    private final PlayerRepository playerRepository;
+
+    @Override
+    public Mono<UserDetails> findByUsername(String username) {
+        return playerRepository.findByNickName(username)
+                .filter(player -> !player.getIsDeleted())
+                .map(player -> new PlayerDetails(player.getNickName(), player.getPassword(), player.getRoles()));
+    }
+}
