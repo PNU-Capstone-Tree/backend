239c9277fb6e4f6cc6d063410c854eb259aac335
JJimini <min102602@pusan.ac.kr>
2024-09-30 23:23:04 +0900

    fix: DB 사이즈 검사 조건 추가

--- src/main/java/com/tree/tree/player/dto/request/SignUpRequest.java
+++ src/main/java/com/tree/tree/player/dto/request/SignUpRequest.java
@@ -1,6 +1,7 @@
 package com.tree.tree.player.dto.request;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,9 +11,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class SignUpRequest {
 
+    private static final int MAX_NICKNAME_LENGTH = 45;
+    private static final int MAX_PASSWORD_LENGTH = 100;
+
     @NotNull(message = "닉네임을 입력해주세요.")
+    @Size(max = MAX_NICKNAME_LENGTH)
     private String nickName;
 
     @NotNull(message = "비밀번호를 입력해주세요.")
+    @Size(max = MAX_PASSWORD_LENGTH)
     private String password;
 }
--- src/main/java/com/tree/tree/ranking/dto/request/RankingCreateRequest.java
+++ src/main/java/com/tree/tree/ranking/dto/request/RankingCreateRequest.java
@@ -2,6 +2,7 @@ package com.tree.tree.ranking.dto.request;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,7 +13,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RankingCreateRequest {
+
+    private static final int MAX_NICKNAME_LENGTH = 45;
+
     @NotNull(message = "닉네임을 입력해주세요.")
+    @Size(max = MAX_NICKNAME_LENGTH)
     private String nickName;
 
     @NotNull(message = "점수를 입력해주세요.")
