b8d2049a8e186083badd9e6e5fcd7f90693814bb
JJimini <min102602@pusan.ac.kr>
2024-10-25 00:18:01 +0900

    fix: `is_delete = false` 인 랭킹만 조회하도록 수정

--- src/main/java/com/tree/tree/ranking/repository/RankingRepository.java
+++ src/main/java/com/tree/tree/ranking/repository/RankingRepository.java
@@ -2,11 +2,13 @@ package com.tree.tree.ranking.repository;
 
 import com.tree.tree.ranking.Ranking;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface RankingRepository extends ReactiveCrudRepository<Ranking, Long> {
+    @Query("SELECT * FROM ranking WHERE is_deleted = false")
     Flux<Ranking> findAllByOrderByRankNumberAsc(Pageable pageable);
     Flux<Ranking> findAllByOrderByMaxScoreAsc();
     Mono<Ranking> findByPlayerId(final Long playerId);
